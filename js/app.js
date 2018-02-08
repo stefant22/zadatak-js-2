72319c0cb115dc25a58b253fc36eeb40ad8ac629
stefant22 <stefantovilovic22@gmail.com>
2018-02-08 17:25:41 +0100

    update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,97 @@
 
+
+window.onload=function(){
+
+document.getElementById("posaoJ").onmouseover=function(){
+    posaoJ.style="background-color:#C8E;"
+
+}
+document.getElementById("posaoJ").onmouseout=function(){
+    posaoJ.style="background-color:''"
+
+}
+
+
+
+document.getElementById("posaoD").onmouseover=function(){
+    posaoD.style="background-color:#F99;"
+
+}
+document.getElementById("posaoD").onmouseout=function(){
+    posaoD.style="background-color:''"
+
+}
+
+document.getElementById("posaoT").onmouseover=function(){
+    posaoT.style="background-color:#FF9;"
+
+}
+document.getElementById("posaoT").onmouseout=function(){
+    posaoT.style="background-color:''"
+
+}
+
+
+
+document.getElementById("sarJ").onmouseover=function(){
+    sarJ.style="background-color:#FF9;"
+
+}
+document.getElementById("sarJ").onmouseout=function(){
+    sarJ.style="background-color:''"
+
+}
+
+
+document.getElementById("sarD").onmouseover=function(){
+    sarD.style="background-color:#C8E;"
+
+}
+document.getElementById("sarD").onmouseout=function(){
+   sarD.style="background-color:''"
+
+}
+
+document.getElementById("sarT").onmouseover=function(){
+    sarT.style="background-color:#F99;"
+
+}
+document.getElementById("sarT").onmouseout=function(){
+    sarT.style="background-color:''"
+
+}
+
+
+
+document.getElementById("konJ").onmouseover=function(){
+    konJ.style="background-color:#F99;"
+
+}
+document.getElementById("konJ").onmouseout=function(){
+    konJ.style="background-color:''"
+
+}
+
+document.getElementById("konD").onmouseover=function(){
+    konD.style="background-color:#FF9;"
+
+}
+document.getElementById("konD").onmouseout=function(){
+    konD.style="background-color:''"
+
+}
+
+
+
+
+
+
+}
+
+
+/*
+
+
 $(document).ready(function(){
   
 
@@ -23,29 +116,10 @@ $(document).ready(function(){
                 });
     
 
+     
 
 
 
   
 
-$("#posaoToggle").click(function(){
-    $("#posao").toggle();
-    $("#saradnici").hide();
-    $("#kontakt").hide();
-});
-
-$("#saradniciToggle").click(function(){
-    $("#saradnici").toggle();
-    $("#posao").hide();
-    $("#kontakt").hide();
-});
-
-$("#kontaktToggle").click(function(){
-    $("#kontakt").toggle();
-    $("#posao").hide();
-    $("#saradnici").hide();
-});
-
-});
-
-
+*/
